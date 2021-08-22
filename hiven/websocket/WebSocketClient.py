4332a3b503bca502a5b1ceacdd9ce3ea6dfc8913
Kevin Thomas <kevin.jt2007@gmail.com>
2021-08-22 00:35:58 -0700

    Added WebsocketCliebnt

---
+++ hiven/websocket/WebSocketClient.py
@@ -0,0 +1,56 @@
+import json
+import asyncio
+import aiohttp
+
+from .types import User
+
+
+class WebSocketClient:
+    def __init__(self, session: aiohttp.ClientSession, client):
+
+        self._session = session
+        self._client = client
+
+    async def connect(self, token: str, bot: bool = True):
+        """Connects to the Hiven WebSocket Swarm"""
+
+        self._token = token
+        self._ws = await self._session.ws_connect(
+            "wss://swarm.hiven.io/socket?encoding=json&compression=text_json"
+        )
+
+        while True:
+            msg = await self._ws.receive()
+
+            print(msg.type)
+
+            if msg.type == aiohttp.WSMsgType.TEXT:
+
+                message = json.loads(msg.data)
+                print(message)
+
+                if message["op"] == 1:
+                    await self._ws.send_json(
+                        {"op": 2, "d": {"token": ("Bot " if bot else "") + self._token}}
+                    )
+                    asyncio.create_task(self.heartbeat(message["d"]["hbt_int"] // 1000))
+            elif msg.type == aiohttp.WSMsgType.CLOSE:
+                print("closing")
+                print(msg)
+            elif msg.type == aiohttp.WSMsgType.CLOSED:
+                print("closed")
+                print(msg)
+                break
+            elif msg.type == aiohttp.WSMsgType.ERROR:
+                print("error")
+                print(msg)
+                print("\n\n")
+                break
+
+    async def heartbeat(self, interval: int):
+        """Start sending heartbeat websocket messages at the specified interval"""
+
+        while True:
+            print("sending heartbeat...")
+            await self._ws.send_json({"op": 3})
+            await asyncio.sleep(interval)
