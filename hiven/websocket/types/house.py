926646ecc2b81af6ae45c05f35273a0a1dcc6f27
Kevin Thomas <kevin.jt2007@gmail.com>
2021-08-22 23:03:21 -0700

    Added basic init of House

--- hiven/websocket/types/house.py
+++ hiven/websocket/types/house.py
@@ -1,4 +1,13 @@
+from ..schemas import House as HouseSchema
+
+
 class House:
-    def __init__(self):
-        self.channels = None
-        self.members = None
+    def __init__(self, house: HouseSchema):
+        self.id = house.get("id")
+        self.name = house.get("name")
+        self.icon = house.get("icon")
+        self.owner_id = house.get("owner_id")
+        self.banner = house.get("banner")
+        self.default_permissions = house.get("default_permissions")
+        self.members = house.get("members")
+        self.rooms = house.get("rooms")
