aa780367e691978137cbd690efea25aeeccd5512
Kevin Thomas <kevin.jt2007@gmail.com>
2021-08-23 12:23:45 -0700

    Added Room type initialization

---
+++ hiven/websocket/types/room.py
@@ -0,0 +1,14 @@
+from ..schemas import Room as RoomSchema
+
+
+class Room:
+    def __init__(self, room: RoomSchema):
+        self.id = room.get("id")
+        self.house_id = room.get("house_id")
+        self.type = room.get("type")
+        self.emoji = room.get("emoji")
+        self.name = room.get("name")
+        self.position = room.get("position")
+        self.last_message_id = room.get("last_message_id")
+        self.description = room.get("description")
+        self.default_permission_override = room.get("default_permission_override")
