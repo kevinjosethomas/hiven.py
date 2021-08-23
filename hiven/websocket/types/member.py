e9c8cc3d1495bf257eaae8616b1585874ba5815b
Kevin Thomas <kevin.jt2007@gmail.com>
2021-08-23 13:26:16 -0700

    Added Member type initialization

---
+++ hiven/websocket/types/member.py
@@ -0,0 +1,14 @@
+from .user import User
+from ..schemas import Member
+
+
+class Member:
+    def __init__(self, member: Member):
+        self.id = member.get("id")
+        self.user_id = member.get("user_id")
+        self.house_id = member.get("house_id")
+        self.user = User(member.get("user"))
+        self.roles = member.get("roles")
+        self.presence = member.get("presence")
+        self.last_permission_update = member.get("last_permission_update")
+        self.joined_at = member.get("joined_at")
