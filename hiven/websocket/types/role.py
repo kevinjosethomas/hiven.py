e7e01fcc7f3f8d5b5daf5901f0508c90794f6b3f
Kevin Thomas <kevin.jt2007@gmail.com>
2021-08-23 13:49:00 -0700

    Added Role type initialization

---
+++ hiven/websocket/types/role.py
@@ -0,0 +1,13 @@
+from ..schemas import Role as RoleSchema
+
+
+class Role:
+    def __init__(self, role: RoleSchema):
+        self.id = role.get("id")
+        self.house_id = role.get("house_id")
+        self.name = role.get("name")
+        self.position = role.get("position")
+        self.level = role.get("level")
+        self.color = role.get("color")
+        self.allow = role.get("allow")
+        self.deny = role.get("deny")
