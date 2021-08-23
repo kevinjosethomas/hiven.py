b9bab50ef024b577783bc5a335a9cc3de8a2f56d
Kevin Thomas <kevin.jt2007@gmail.com>
2021-08-23 13:48:49 -0700

    Added more typehinting

--- hiven/websocket/schemas/house.py
+++ hiven/websocket/schemas/house.py
@@ -1,15 +1,16 @@
 from typing import List
 
+from .role import Role
 from .room import Room
 from .member import Member
 
 
 class House:
     id: str
-    name: str
     icon: str
     owner_id: str
     banner: str
     default_permissions: int
-    members: List[Member]
     rooms: List[Room]
+    roles: List[Role]
+    members: List[Member]
