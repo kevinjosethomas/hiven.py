3d5a9e618c0acb1987dd8afbb877dcbc64838cdc
Kevin Thomas <kevin.jt2007@gmail.com>
2021-08-23 13:32:20 -0700

    Added optional emoji

--- hiven/websocket/types/room.py
+++ hiven/websocket/types/room.py
@@ -7,7 +7,7 @@ class Room:
         self.id = room.get("id")
         self.house_id = room.get("house_id")
         self.type = room.get("type")
-        self.emoji = ChannelEmoji(room.get("emoji"))
+        self.emoji = ChannelEmoji(room.get("emoji")) if room.get("emoji") else None
         self.name = room.get("name")
         self.position = room.get("position")
         self.last_message_id = room.get("last_message_id")
