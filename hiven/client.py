9384d620a26700bc0eb07cfd3d6e38dad99f5403
Kevin Thomas <kevin.jt2007@gmail.com>
2021-08-24 16:47:26 -0700

    Added dispatch_event function, checked for coroutines

--- hiven/client.py
+++ hiven/client.py
@@ -1,7 +1,9 @@
-from typing import Coroutine
+import inspect
 import aiohttp
 import asyncio
+from typing import Coroutine
 
+from .errors import EventHandlerError
 from .websocket import WebSocketClient
 
 
@@ -11,6 +13,7 @@ AVAILABLE_EVENTS = ["ready"]
 class Client:
     def __init__(self, bot: bool = True):
         self.bot = bot
+        self.is_ready = False
         self._loop = asyncio.get_event_loop()
 
         self.event_handlers = {}
@@ -22,12 +25,28 @@ class Client:
     def event(self, awaitable: Coroutine):
         """Decorator to recognize a function as an event handler"""
 
-        print(awaitable.__name__)
+        if not inspect.iscoroutinefunction(awaitable):
+            return TypeError(f"Expected a coroutine function, received {type(awaitable)}")
+
         event_name = awaitable.__name__.replace("on_", "")
         if event_name not in AVAILABLE_EVENTS:
-            print("invalid event")
+            raise EventHandlerError(f"Invalid event handler name, received {event_name}")
+
+        if self.event_handlers.get(event_name):
+            self.event_handlers[event_name].append(awaitable)
+        else:
+            self.event_handlers[event_name] = [awaitable]
 
         return awaitable
+
+    async def dispatch_event(self, event: str, args: tuple = (), kwargs: dict = {}):
+        """Dispatches the specified event with the provided arguments"""
+
+        if self.event_handlers.get(event):
+            handlers = [
+                event_handler(*args, **kwargs) for event_handler in self.event_handlers[event]
+            ]
+            await asyncio.gather(*handlers)
 
     def run(self, token: str):
         """Runs the client with the provided token"""
