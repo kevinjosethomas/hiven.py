7aa6607cf98087d1b960c887faef3e283d07cbec
Kevin Thomas <kevin.jt2007@gmail.com>
2021-08-29 20:02:52 -0700

    Added cached house and room

--- hiven/types/message.py
+++ hiven/types/message.py
@@ -36,8 +36,11 @@ class Message:
         self.exploding = message.get("exploding")
         self.exploding_age = message.get("exploding_age")
         self.member = Member(message.get("member"), client)
-        self.author_id = User(message.get("author_id"), client)
-        self.author = message.get("author")
+        self.author_id = message.get("author_id")
+        self.author = User(message.get("author"), client)
         self.device_id = message.get("device_id")
         self.bucket = message.get("bucket")
         self.timestamp = message.get("timestamp")
+
+        self.house = client.houses.get(self.house_id)
+        self.room = self.house.rooms.get(self.room_id) if self.house else None
