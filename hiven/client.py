88418ca31b2ca198f9103713d8257fb19097d03d
Kevin Thomas <kevin.jt2007@gmail.com>
2021-08-31 11:08:47 -0700

    Added default arguments

--- hiven/client.py
+++ hiven/client.py
@@ -48,6 +48,9 @@ class Client:
     async def dispatch_event(self, event: str, args: tuple = None, kwargs: dict = None):
         """Dispatches the specified event with the provided arguments"""
 
+        args = args if args else ()
+        kwargs = kwargs if kwargs else {}
+
         if self.event_handlers.get(event):
             handlers = [event_handler(*args, **kwargs) for event_handler in self.event_handlers[event]]
             results = await asyncio.gather(*handlers, return_exceptions=True)
@@ -55,7 +58,7 @@ class Client:
             for handler, result in zip(handlers, results):
                 if isinstance(result, Exception):
                     self._logger.error(
-                        f"Exception occurred in {handler.__module__}.{handler.__qualname__}:\n{format_exception(result)}"
+                        f"Exception occurred in {handler.__qualname__}:\n{format_exception(result)}"
                     )
 
     def run(self, token: str):
