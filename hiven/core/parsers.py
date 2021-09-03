fa7e73ad6b6379d5b5aa443adad4b1c55363b201
Kevin Thomas <kevin.jt2007@gmail.com>
2021-09-03 13:03:46 -0700

    Added String, Integer, Boolean parsers

---
+++ hiven/core/parsers.py
@@ -0,0 +1,42 @@
+from hiven.errors import ArgumentParsingError
+
+
+class Parser:
+    """Base class for all parsers"""
+
+    @staticmethod
+    def parse(ctx, argument: str):
+        pass
+
+    pass
+
+
+class String(Parser):
+    """Class to parse string arguments"""
+
+    def parse(ctx, argument):
+        return str(argument.strip())
+
+
+class Integer(Parser):
+    """Class to parse integer arguments"""
+
+    def parse(ctx, argument):
+        try:
+            return int(argument.strip())
+        except ValueError:
+            raise ArgumentParsingError()
+
+
+class Boolean(Parser):
+    """Class to parse boolean arguments"""
+
+    def parse(ctx, argument):
+        arg = argument.lower().strip()
+
+        if arg in ["true", "yes", "on"]:
+            return True
+        elif arg in ["false", "no", "off"]:
+            return False
+        else:
+            raise ArgumentParsingError()
